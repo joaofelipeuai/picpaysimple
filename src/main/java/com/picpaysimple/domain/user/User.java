674233978b9032586a6215581f3e7347d2560581
João Felipe <141428226+joaofelipeuai@users.noreply.github.com>
2023-08-15 11:51:39 -0300

    feat: create User entity

---
+++ src/main/java/com/picpaysimple/domain/user/User.java
@@ -0,0 +1,32 @@
+package com.picpaysimple.domain.user;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lasName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
