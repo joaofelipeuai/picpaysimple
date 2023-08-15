a5a4da535b04188cf2c1f058e2d41edc2f70af02
João Felipe <141428226+joaofelipeuai@users.noreply.github.com>
2023-08-15 16:11:13 -0300

    feat: create Services

---
+++ src/main/java/com/picpaysimple/services/NotificationService.java
@@ -0,0 +1,31 @@
+package com.picpaysimple.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.picpaysimple.domain.user.User;
+import com.picpaysimple.dtos.NotificationDTO;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception{
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+/*      ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("Erro ao enviar a notificação");
+            throw new Exception("Serviço de notificação indisponível");
+        }  
+        */  
+
+        System.out.println("Notificação enviada ao usuário!");
+    }
+}
---
+++ src/main/java/com/picpaysimple/services/UserService.java
@@ -0,0 +1,46 @@
+package com.picpaysimple.services;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.picpaysimple.domain.user.User;
+import com.picpaysimple.domain.user.UserType;
+import com.picpaysimple.dtos.UserDTO;
+import com.picpaysimple.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo Lojista não está autorizado a realizar esta transação!");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente!");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception{
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado!"));
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
