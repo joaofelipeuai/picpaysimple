2f64b5e035f4d67820b3b5cc2b038a71f7408678
João Felipe <141428226+joaofelipeuai@users.noreply.github.com>
2023-08-15 11:51:59 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimple/domain/transaction/Transaction.java
@@ -0,0 +1,33 @@
+package com.picpaysimple.domain.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.picpaysimple.domain.user.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User Sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User Receiver;
+    private LocalDateTime timestamp;
+}   
